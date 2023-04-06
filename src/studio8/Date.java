2b5de94fea0601d3c9efc6e79cca42b0e5643344
jeffreycisneros <jeffreycisneros@localhost>
2023-04-06 12:21:14 -0500

    tadaaa

--- src/studio8/Date.java
+++ src/studio8/Date.java
@@ -1,10 +1,68 @@
 package studio8;
 
-public class Date {
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.Objects;
 
+public class Date {
+private int month;
+private int day;
+private int year;
+private boolean holiday;
+/**constructor for date
+ * 
+ * @param month
+ * @param day
+ * @param year
+ * @param holiday
+ */
+
+public Date (int month, int day, int year, boolean holiday) {
+	this.month = month;
+	this.day = day;
+	this.year = year;
+	this.holiday = holiday;
+	
+}
+public String toString() {
+	return month + "/" + day + "/" + year;
+}
 
     public static void main(String[] args) {
-
+    	Date d1 = new Date(4, 6, 2023, false);
+    	System.out.println(d1);
+    	Date d2 = new Date(5, 30, 2004, false);
+    	Date d3 = new Date(8, 24, 2019, false);
+    	Date d4 = new Date(12, 25, 2013, true);
+    	Date d5 = new Date(3, 13, 2020, true);
+    	LinkedList<Date> list = new LinkedList<Date>();
+    	list.add(d1); 
+    	list.add(d1);
+    	list.add(d3); 
+    	list.add(d4); 
+    	list.add(d5); 
+    	System.out.println(list);
+    	HashSet<Date> set = new HashSet<Date>();
+    	list.add(d1); 
+    	list.add(d1);
+    	list.add(d3); 
+    	System.out.println(set);
     }
+    
+	@Override
+	public int hashCode() {
+		return Objects.hash(day, month, year);
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Date other = (Date) obj;
+		return day == other.day && month == other.month && year == other.year;
+	}
 
 }
--- src/studio8/Time.java
+++ src/studio8/Time.java
@@ -1,11 +1,55 @@
 package studio8;
 
+import java.util.Objects;
 
 public class Time {
+private int hour;
+private int minute;
+private boolean am;
+/** constructor for time
+ * 
+ * @param hour
+ * @param minute
+ * @param am
+ */
+public Time (int hour, int minute, boolean am) {
+	this.hour = hour;
+	this.minute = minute;
+	this.am= am;
+}
 
+public String toString() {
+	if (hour < 12) {
+		return hour + ":" + minute + " am";
+	}
+	else {
+		int newHour = hour - 12;
+		return newHour + ":" + minute + " pm";
+	}
+}
 	public static void main(String[] args) {
- 
-    	
+		Time t1 = new Time(3, 20, true);
+		System.out.println(t1);
+		Time t2 = new Time(15, 30, false);
+		System.out.println(t2);
+		System.out.println(t1.equals(t2));
     }
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(hour, minute);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Time other = (Time) obj;
+		return hour == other.hour && minute == other.minute;
+	}
 
 }
